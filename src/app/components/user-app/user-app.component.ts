d83e5e813e20c855ec32571e3870b62215908a6c
AlexSanti33 <santiago.jav27@gmail.com>
2024-05-26 00:13:38 -0600

    Se agrega acciones actualizar y visualizar en modal

--- src/app/components/user-app/user-app.component.ts
+++ src/app/components/user-app/user-app.component.ts
@@ -3,6 +3,7 @@ import { User } from '../../models/user';
 import { UserService } from '../../services/user.service';
 import { UserComponent } from '../user/user.component';
 import { FormUserComponent } from '../form-user/form-user.component';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'user-app',
@@ -17,6 +18,8 @@ export class UserAppComponent implements OnInit {
   users: User[] = [];
   userSelected: User;
 
+  open: boolean = false;
+
   constructor(private service: UserService){
 
     this.userSelected = new User();
@@ -27,16 +30,52 @@ export class UserAppComponent implements OnInit {
   }
 
   addUser(user: User): void{
-    this.users=[...this.users,{...user}];
+    if(user.id > 0){
+      this.users = this.users.map(u=> (u.id == user.id)?{ ...user } : u);
+    }else {
+      this.users=[...this.users,{...user,id: new Date().getTime()}];
+    }
+
+  Swal.fire({
+    title: "Guardado",
+    text: "Se ha guardado correctamente",
+    icon: "success"
+  });
+    this.userSelected = new User();
   }
 
   removeUser(id: number): void {
-    this.users = this.users.filter(user => user.id != id);
+
+    Swal.fire({
+      title: "Seguro que quiere eliminar?",
+      text: "Cuidado el usuario sera eliminado del sistema",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Si"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.users = this.users.filter(user => user.id != id);
+        Swal.fire({
+          title: "Eliminado!",
+          text: "Usuario eliminado con exito",
+          icon: "success"
+        });
+      }
+    });
+
+
   }
 
   setSelectedUser(userRow: User): void{
     this.userSelected = {... userRow}
+    this.open=true;
 
+  }
+
+  setOpen(): void {
+    this.open = !this.open;
   }
 
 }
